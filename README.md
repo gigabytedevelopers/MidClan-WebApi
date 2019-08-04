a223167015f7cd0879d372810e01f39e099e1db7
Onuoha <martinsvictor.nd@gmail.com>
2019-08-04 21:15:15 +0100

    updated readme & folder structure

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # MidClan-WebApi
+
+## Git WorkFlow
+
+### Base branch: `master`
+
+### Work branch: `develop`
+
+To work on a new chore/feature/bug
+
+- checkout to master
+- pull from master remote
+- checkout to new feature/fix/chore branch (git checkout -b feature/xxx-xxx or git checkout -b chore/setup)
+- do your magic
+- merge and commit changes on that branch (git add . && git commit -m "finished feature xxx")
+- push branch to remote (git push origin feature/xxx-xxx)
+- checkout to `develop` branch (git checkout develop)
+- pull from develop remote (git pull origin develop)
+- merge in new feature/fix/chore branch into `develop` (git merge feature/xxx-xxx)
+- push `develop` to remote (git push origin develop)
+- then, create pull request from `develop` to `master`
--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,6 +17,7 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,14 +25,15 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
-  res.locals.message = err.message;
+  const { message } = err;
+  res.locals.message = message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
